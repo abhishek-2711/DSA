2528de4ccf79a38979a0684eb05846c4bc1bfb5f
Abhishek Rathod <rathod.abhishek2021@gmail.com>
2024-01-04 21:40:17 +0530

    Add C++ Solution for DSA Problem-1 on Day-1

---
+++ Day-1/Problem-1/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0, high = n-1;
+        int ans = INT_MAX;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            // Right Portion is sorted
+            if(nums[mid] < nums[high]){
+                ans = min(ans,nums[mid]);
+                high = mid - 1;
+            }
+            else{
+                ans = min(ans,nums[low]);
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
